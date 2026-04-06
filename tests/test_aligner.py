1614a2774de61d97a9ac80c87de6e2985de7613d
Yitong Ma <yitongma7@gmail.com>
2026-04-06 01:31:28 -0700

    Update CI workflow configuration

--- tests/test_aligner.py
+++ tests/test_aligner.py
@@ -1,5 +1,6 @@
 import pytest
 from pw_align import correct_seq_mt
+from random import choices
 
 def test_correct_seq():
     ref_list = ['TGAAGCCAATAGTGATTTGGA', 'GAACCGGGCCTAAGCTTTCCG', 'GAGCTAAAATTGCAGCTAGTG']
@@ -11,4 +12,9 @@ def test_correct_seq():
 
     assert correct_seq_mt(ref_list, ['TGAAGCCTATAGGATTTGGA'], 2, 1) == ['TGAAGCCAATAGTGATTTGGA']
     assert correct_seq_mt(ref_list, ['TGAAGCCTATAGGATTTGGA'], 1, 1) == ['']
+
+def test_correct_seq_auto():
+    ref_list = []
+    for _ in range(100):
+        ref_list.append(''.join(choices('ACGT', k=22)))
 
